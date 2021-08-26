df67684f92b8840ca6d358a060ebd4a9d4b1c0e5
smate <smateusz49@interia.eu>
2021-08-26 18:42:07 +0200

    assetPath don t work with public folder 2

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -2,8 +2,6 @@ import styled from '@emotion/styled';
 import full_logo1 from '@/public/images/full_logo1.svg';
 import { colors } from '@/src/styles';
 
-const prefix = '/stronka';
-
 export const Header = styled.div`
     background: ${colors.header_color};
     color: ${colors.white};
@@ -19,7 +17,7 @@ export const Full_Logo = styled.div`
     width: 168px;
     height: 50px;
     margin: 28px;
-    background-image: url(${prefix + full_logo1.src});
+    background-image: url(${full_logo1.src});
     position: absolute;
     top: 0;
     left: 0;
