88471574a875b28754b9404dfa64ce7a9c6fde74
smate <smateusz49@interia.eu>
2021-08-26 15:46:12 +0200

    check 14

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPlugins = require('next-compose-plugins');
 module.exports = withPlugins([
     {
         trailingSlash: true,
-        basePath: 'https://SMATEO49.github.io',
-        assetPrefix: '/stronka',
+        basePath: '',
+        assetPrefix: 'https://SMATEO49.github.io/stronka',
     },
 ]);
