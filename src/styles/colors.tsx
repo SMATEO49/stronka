ef1bad670bf13668654f8e477b40d94879a892b3
smate <smateusz49@interia.eu>
2021-08-26 20:54:03 +0200

    resolving border on logo4.1

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -1,7 +1,7 @@
 export const colors = {
     white: '#ffffff',
     header_color: '#15159b',
-    footer_color: '#f6fafd',
+    footer_color: '#000',
     footer_text: '#78a0fa',
     rights_text: '#7e849d',
 } as const;
