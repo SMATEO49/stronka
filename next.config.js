3c123de20a534bb98b8fc2264e4e01c91602cf78
smate <smateusz49@interia.eu>
2021-08-26 15:15:51 +0200

    check 4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    trailingSlash: true,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+module.exports = (phase, { defaultConfig }) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        trailingSlash: true,
+        basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+        assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    };
+    return nextConfig;
 };
-
-module.exports = nextConfig;
