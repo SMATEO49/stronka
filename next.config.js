a9937939cdf7b8deacb90e2c9d6d1335b2e99814
smate <smateusz49@interia.eu>
2021-08-26 15:25:25 +0200

    check 7

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 const withPlugins = require('next-compose-plugins');
 
-module.exports = (phase, { defaultConfig }) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-            trailingSlash: true,
-            basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-            assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-        };
-    }
-
-    return {
+module.exports = withPlugins([
+    {
         trailingSlash: true,
         basePath: process.env.NEXT_PUBLIC_BASE_PATH,
         assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-    };
-};
+    },
+]);
