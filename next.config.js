2079fea2dbfb69b066919edc6c159b94c4af210f
smate <smateusz49@interia.eu>
2021-08-26 18:25:58 +0200

    check 23

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPlugins = require('next-compose-plugins');
 module.exports = withPlugins([
     {
         trailingSlash: true,
-        basePath: '',
+        basePath: '/stronka',
         assetPath: '/stronka',
     },
 ]);
