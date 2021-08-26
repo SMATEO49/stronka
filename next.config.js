03aa4518bf98464b53f43722b5778b5a4a52f1a4
smate <smateusz49@interia.eu>
2021-08-26 19:03:13 +0200

    resolve by image part1

--- next.config.js
+++ next.config.js
@@ -5,5 +5,8 @@ module.exports = withPlugins([
         trailingSlash: true,
         basePath: process.env.NEXT_PUBLIC_BASE_PATH, //potrzebny
         assetPath: process.env.NEXT_PUBLIC_BASE_PATH, //nie dziala
+        devIndicators: {
+            autoPrerender: false,
+        },
     },
 ]);
