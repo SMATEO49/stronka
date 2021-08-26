7d6916422419fd63d43ed515eae449145c4e77f3
smate <smateusz49@interia.eu>
2021-08-26 15:18:45 +0200

    check 5

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
 module.exports = (phase, { defaultConfig }) => {
-    /**
-     * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            trailingSlash: true,
+            basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+            assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+        };
+    }
+
+    return {
         trailingSlash: true,
         basePath: process.env.NEXT_PUBLIC_BASE_PATH,
         assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     };
-    return nextConfig;
 };
