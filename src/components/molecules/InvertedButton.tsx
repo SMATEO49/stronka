7457a22ad3012a4d84d7cfdd6e82e621de90ac6f
susz 49 <smateusz49@interia.eu>
2021-09-02 21:21:38 +0200

    footer-full 1.5

--- src/components/molecules/InvertedButton.tsx
+++ src/components/molecules/InvertedButton.tsx
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 
 const Icon = styled.div<{ imagIcon: any }>`
     background-image: url(${({ imagIcon }) => imagIcon});
+    height: 50;
+    width: 50;
     :hover {
         filter: invert(3%);
         box-shadow: 6px 6px 6px -1px black;
