22cdbae7f14e7bb2216fa6a3d217b2ea30c88248
susz 49 <smateusz49@interia.eu>
2021-09-02 19:32:03 +0200

    logo-borderless 2

--- src/components/molecules/Logo.tsx
+++ src/components/molecules/Logo.tsx
@@ -3,19 +3,20 @@ const full_logo1 = 'images/full_logo1.svg';
 const Full_Logo = styled.img`
     width: 168px;
     height: 50px;
-    border-width: 10px;
-    background-image: url(${full_logo1});
-    position: absolute;
-    top: 0;
-    left: 0;
+    border: none;
+    src: url(${full_logo1});
+    margin: 0;
 `;
 export const Logo = () => {
     return (
-        <img
-            src={full_logo1}
-            width="168"
-            height="50"
-            style={{ border: 'none', position: 'absolute', top: '28px', left: '28px', margin: '0' }}
-        />
+        <div>
+            <img
+                src={full_logo1}
+                width="168"
+                height="50"
+                style={{ border: 'none', position: 'fixed', top: '28px', left: '28px', margin: '0' }}
+            />
+            <Full_Logo />
+        </div>
     );
 };
